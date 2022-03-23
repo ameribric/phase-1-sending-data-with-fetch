c10a4b78a0781e514fcba3d8c9e10146a1c97e6e
Amer Ibric <ameribric@gmail.com>
2022-03-23 14:21:47 -0400

    commit tree 2bec1a9c81c8a5c122c09ba337123e675f2da586

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-// Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    });
+
+
+
+
+
+
+
+//POST SYNTAX
+
+// function adoptAnimal(animalObj) {
+//     fetch('htttp://localhost:3000/animalData')
+//     .then(res => res.json())
+//     .then(animalData => animalData.forEach(animal => renderOneAnimal(animal)))
+// }
+
+// fetch('http://localhost:3000/animalData',{
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body:JSON.stringify(animalObj)
+// })
+// .then(res => res.json())
+// .then(animal => console.log(animal))
+
+
+
+
+// //PATCH SYNTAX
+// fetch(`http://localhost:3000/animalData/${animalObj.id}`,{
+//     method: 'PATCH',
+//     headers:{
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(animalObj)
+// })
+// .then(res => res.json())
+// .then(animal => console.log(animal))
+
+// //DELETE SYNTAX
+
+// card.querySelector('#set_free').addEventListener('click', () => {
+//     card.remove()
+//     deleteAnimal(animal.id)
+// })
+
+// function deleteAnimal(id){
+//     fetch(`http://localhost:3000/animalData/${id}`,{
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then(res => res.json())
+//     .then(animal => console.log(animal))
+// }
